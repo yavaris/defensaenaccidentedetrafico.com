e2a0975a23ad19fd9771a9522972fbe4d7b1582d
Francisco Cerezo Leal <francisco@costasur.com>
2013-03-02 22:19:09 +0100

    Construyendo fichero de configuracion

---
+++ html/code/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$arraySocialLinks = array
+                        (
+                            "facebook" => array("name" => "Facebook", "link" => "#", "target" => 1),
+                            "twitter"  => array("name" => "Twitter", "link" => "https://twitter.com/abogado_trafico", "target" => 1),
+                            "gplus"    => array("name" => "Google+", "link" => "#", "target" => 1),
+                            "phone"    => array("name" => "+34.666.666.666", "link" => "tel:+34.666.666.666", "target" => 0),
+                            "mail"     => array("name" => "info@defensaaccidentestrafico.com", "link" => "mailto:info@defensaaccidentestrafico.com", "target" => 0)
+                        );
+?>
